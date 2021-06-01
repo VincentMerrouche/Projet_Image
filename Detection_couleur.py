5767447e7ddbf284730bea13c47ede84002cf7d0
Emma5099 <emma.genthon@cpe.fr>
2021-06-01 11:23:29 +0200

    maj

---
+++ Detection_couleur.py
@@ -0,0 +1,11 @@
+#Importations des librairies
+import numpy as np 
+import cv2 
+import matplotlib.pyplot as plt 
+from PIL.Image import * 
+
+#Lecture de l'image 
+Chemin="Images_Resistance/resistance_1.png"
+
+Image_Couleur=open(Chemin)
+Image_Forme= cv2.imread(Chemin) 
