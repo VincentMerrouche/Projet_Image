8c6383ca4e4dc1fcc6492246421b58e18d0768d9
VincentMerrouche <79834718+VincentMerrouche@users.noreply.github.com>
2021-06-01 13:34:34 +0200

    détourage

---
+++ Détourage/détourage.py
@@ -0,0 +1,45 @@
+
+###Code###
+#Ce code met en oeuvre la methode de segmentation du snake
+##########
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+from cv2 import cv2
+from PIL import Image
+
+
+#récupération des bandes en noir
+colorImage = cv2.imread("Images_Resistance/resistance_1.png",0)
+plt.figure(2)
+plt.imshow(colorImage)
+plt.show()
+
+
+
+
+
+
+
+ 
+
+# Display the Original Image
+
+colorImage.show()
+
+ 
+
+# Display the Image rotated by 45 degrees
+
+rotated.show()
+
+ 
+
+# Display the Image rotated by 90 degrees
+
+
+
+
+
+
+
